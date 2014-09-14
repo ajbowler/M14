5a2cd770fd21fc52bbe09c47f3314663b0748665
Alex Lende <ajlende@gmail.com>
2014-09-14 16:18:16 -0500

    Included requireJS, reactJS and jQuery

---
+++ WWW/js/main.js
@@ -0,0 +1,10 @@
+require.config({
+    'paths': {
+        'jquery': 'lib/jquery',
+        'react': 'lib/react'
+    }
+});
+
+require(['jquery', 'react'], function($, React) {
+    // Entry point for the program
+});
