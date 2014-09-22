567ce38efd9eaec983e226c834af46f6b9b37d3e
Alex Lende <ajlende@iastate.edu>
2014-09-22 09:38:55 -0500

    Attempting to load the JSX Plugin with requireJS

--- WWW/js/main.js
+++ WWW/js/main.js
@@ -1,10 +1,17 @@
 require.config({
     'paths': {
         'jquery': 'lib/jquery',
-        'react': 'lib/react'
+        'react': 'lib/react',
+        'JSXTransformer': 'lib/JSXTransformer',
+        'jsx': 'lib/jsx'
+    },
+    'jsx': {
+        'fileExtension': '.jsx'
     }
 });
 
 require(['jquery', 'react'], function($, React) {
     // Entry point for the program
+    // React.renderComponent(<Hello />, document.getElementById('hello'));
+    React.renderComponent(React.DOM.h1(null, 'Hello, world!'),document.getElementById('hello'));
 });
