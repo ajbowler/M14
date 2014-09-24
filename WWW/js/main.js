f49690fab441c7b4259677ab83e87e93b7249c54
Andrew Bowler <ajbowler@iastate.edu>
2014-09-24 18:52:26 -0500

    Loading React and jQuery with Require. Haven't figured out how to use JSX with Require yet.

--- WWW/js/main.js
+++ WWW/js/main.js
@@ -1,10 +1,45 @@
 require.config({
     'paths': {
         'jquery': 'lib/jquery',
-        'react': 'lib/react'
+        'react': 'lib/react',
+        'jsx': 'lib/jsx',
+        'JSXTransformer': 'lib/JSXTransformer'
+    },
+
+    'jsx': {
+    	'fileExtension': '.jsx'
     }
 });
 
 require(['jquery', 'react'], function($, React) {
     // Entry point for the program
+   	var helloText = React.createClass({
+   		render: function() {
+   			return (
+   				React.DOM.div({className: "helloText"},
+   				"Hello, world! React is cool!")
+   			);
+   		}
+   	});
+
+   	var helloTextJQuery = React.createClass({
+   		render: function() {
+   			return (
+   				React.DOM.div({className: "helloText"},
+   				"Hello, world! React is cool! And it works with jquery!")
+   			);
+   		}
+   	});
+
+   	React.renderComponent(
+   		helloText(null),
+   		document.getElementById('content')
+   	);
+
+   	var jqueryDiv = $('#content_jquery').get(0);
+
+   	React.renderComponent(
+   		helloTextJQuery(null),
+   		jqueryDiv
+   	);
 });
