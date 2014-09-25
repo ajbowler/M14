fe1c759016d0525bad618842f8b1a2f8790e174b
Andrew Bowler <ajbowler@iastate.edu>
2014-09-24 23:16:10 -0500

    No more Require, everything builds with Browserify and Reactify now, just have to kick off a build command, will figure out how to automate later. The JSX FINALLY WORKS. UGH.

--- WWW/js/main.js
+++ WWW/js/main.js
@@ -1,45 +1,17 @@
-require.config({
-    'paths': {
-        'jquery': 'lib/jquery',
-        'react': 'lib/react',
-        'jsx': 'lib/jsx',
-        'JSXTransformer': 'lib/JSXTransformer'
-    },
+/**
+ * @jsx React.DOM
+ */
 
-    'jsx': {
-    	'fileExtension': '.jsx'
-    }
+var React = require('react');
+
+var Hello = React.createClass({
+
+  render: function() {
+    return <div>Hello, {this.props.name}!</div>
+  }
 });
 
-require(['jquery', 'react'], function($, React) {
-    // Entry point for the program
-   	var helloText = React.createClass({
-   		render: function() {
-   			return (
-   				React.DOM.div({className: "helloText"},
-   				"Hello, world! React is cool!")
-   			);
-   		}
-   	});
-
-   	var helloTextJQuery = React.createClass({
-   		render: function() {
-   			return (
-   				React.DOM.div({className: "helloText"},
-   				"Hello, world! React is cool! And it works with jquery!")
-   			);
-   		}
-   	});
-
-   	React.renderComponent(
-   		helloText(null),
-   		document.getElementById('content')
-   	);
-
-   	var jqueryDiv = $('#content_jquery').get(0);
-
-   	React.renderComponent(
-   		helloTextJQuery(null),
-   		jqueryDiv
-   	);
-});
+React.renderComponent(
+  <Hello name="World" />,
+  document.getElementById('hello')
+);
