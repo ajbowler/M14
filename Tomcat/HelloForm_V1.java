b6463f18f6679a59a3dd1b5f61de2398ef59f7c5
nmont <nmont16@gmail.com>
2014-09-30 00:21:19 -0500

    Added testing Hello Forms utilizing POST calls, along with comparrisons for when they are compiled and opened from a server

--- Tomcat/HelloForm_V1.java
+++ Tomcat/HelloForm_V1.java
@@ -4,10 +4,8 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 
 // Extend HttpServlet class
-// Same as HelloFormV1, but implements a POST method
-public class HelloForm extends HttpServlet {
+public class HelloForm_V1 extends HttpServlet {
  
-  // Method to handle GET method request.
   public void doGet(HttpServletRequest request,
                     HttpServletResponse response)
             throws ServletException, IOException
@@ -16,14 +14,14 @@ public class HelloForm extends HttpServlet {
       response.setContentType("text/html");
 
       PrintWriter out = response.getWriter();
-	  String title = "Using GET Method to Read Form Data\nNotice URL is Clean (used doPost): Nick M";
+	  String title = "Using GET Method to Read Form Data\nNotice URL Has Parameters in it: Nick M";
       String docType =
       "<!doctype html public \"-//w3c//dtd html 4.0 " +
       "transitional//en\">\n";
       out.println(docType +
                 "<html>\n" +
                 "<head><title>" + title + "</title></head>\n" +
-                "<body bgcolor=\"#99FFFF\">\n" +
+                "<body bgcolor=\"#CC9966\">\n" +
                 "<h1 align=\"center\">" + title + "</h1>\n" +
                 "<ul>\n" +
                 "  <li><b>First Name</b>: "
@@ -32,11 +30,5 @@ public class HelloForm extends HttpServlet {
                 + request.getParameter("last_name") + "\n" +
                 "</ul>\n" +
                 "</body></html>");
-  }
-  // Method to handle POST method request.
-  public void doPost(HttpServletRequest request,
-                     HttpServletResponse response)
-      throws ServletException, IOException {
-     doGet(request, response);
   }
 }
