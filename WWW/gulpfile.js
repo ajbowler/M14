494bf4d90ed0fd390a4fefa43f402bc4e19f3313
Alex Lende <ajlende@gmail.com>
2014-10-01 02:43:06 -0500

    Created gulp task to run Jest tests

--- WWW/gulpfile.js
+++ WWW/gulpfile.js
@@ -4,40 +4,50 @@ var browserify = require('browserify');
 var del = require('del');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var jest = require('jest-cli');
+var jestConfig = require('./jest.json');
+
+var files = {
+  main_js: ['./js/main.jsx'],
+  jsx: ['./js/*.jsx']
+};
 
 var paths = {
-    main_js: ['./js/main.jsx'],
-    jsx: ['./js/*.jsx']
+  build: './js/build'
 };
 
 gulp.task('clean', function(cb) {
-    del(['build'], cb);
+  del(['build'], cb);
 });
 
 gulp.task('js', ['clean'], function() {
-    // Browserify/bundle the JS.
-    browserify(paths.main_js)
-        .transform(reactify)
-        .bundle()
-        .pipe(source('bundle.js'))
-        .pipe(gulp.dest('./js/build/'));
+  // Browserify/bundle the JS.
+  browserify(files.main_js)
+    .transform(reactify)
+    .bundle()
+   .pipe(source('bundle.js'))
+   .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('test', function() {
+    jest.runCLI({config: jestConfig},jestConfig.rootDir);
 });
 
 gulp.task('dev', ['clean'], function() {
-    browserify({
-        entries: paths.main_js,
-        extensions: ['jsx'],
-        debug: true
-    })
-    .transform(reactify)
-    .bundle()
-    .pipe(source('bundle.js'))
-    .pipe(gulp.dest('./js/build/'));
+  browserify({
+    entries: files.main_js,
+    extensions: ['jsx'],
+    debug: true
+  })
+  .transform(reactify)
+  .bundle()
+  .pipe(source('bundle.js'))
+  .pipe(gulp.dest(paths.build));
 });
 
 // Rerun the task when a file changes
 gulp.task('watch', function() {
-    gulp.watch(paths.jsx, ['dev']);
+  gulp.watch(files.jsx, ['dev']);
 });
 
 // The default task (called when you run `gulp` from cli)
