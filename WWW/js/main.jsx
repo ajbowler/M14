5e8ae4362c9f68be070d4a2d847ffce681899e85
Andrew Bowler <ajbowler@iastate.edu>
2014-10-01 16:49:32 -0500

    Included jQuery and react-bootstrap. Can now render webpage components using React, jQuery, and Bootstrap all at once!

--- WWW/js/main.jsx
+++ WWW/js/main.jsx
@@ -3,8 +3,10 @@
  */
 
 var React = require('react');
+var $ = require('jquery');
 var Hello = require('./components/hello.jsx');
 var CheckboxWithLabel = require('./components/CheckboxWithLabel.jsx');
+var Alert = require('react-bootstrap/Alert');
 
 React.renderComponent(
   <Hello name='World' />,
@@ -14,4 +16,13 @@ React.renderComponent(
 React.renderComponent(
   <CheckboxWithLabel labelOn='Checked' labelOff='Unchecked' />,
   document.getElementById('checkbox')
+);
+
+React.renderComponent(
+	<Alert bsStyle="warning">
+		Check out this awesome react-bootstrap stuff!
+	</Alert>,
+	//non-jQuery is lame!
+	//document.getElementById('alert')
+	$('#alert').get(0)
 );
