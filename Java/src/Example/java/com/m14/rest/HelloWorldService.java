44913c1b9aaf3f041ad3dc21313f4ba8a0a14498
Nick M <nmont@iastate.edu>
2014-10-04 14:32:19 -0500

    added tutorial files

---
+++ Java/src/Example/java/com/m14/rest/HelloWorldService.java
@@ -0,0 +1,18 @@
+package com.m14.rest;
+ 
+import javax.ws.rs.*;
+ 
+@Path("/hello")
+public class HelloWorldService {
+ 
+	@GET
+	@Path("/{param}")
+	public Response getMsg(@PathParam("param") String msg) {
+ 
+		String output = "Jersey say : " + msg;
+ 
+		return Response.status(200).entity(output).build();
+ 
+	}
+ 
+}
