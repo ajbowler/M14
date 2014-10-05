1ccc6427423c8b819331278541d99b2987e7c3b9
Nick M <nmont@iastate.edu>
2014-10-04 19:33:43 -0500

    working RESTfulExample

--- Java/RESTfulExample/src/main/java/com/m14/rest/HelloWorldService.java
+++ Java/RESTfulExample/src/main/java/com/m14/rest/HelloWorldService.java
@@ -4,6 +4,7 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.*;
  
 @Path("/hello")
 public class HelloWorldService {
@@ -17,5 +18,13 @@ public class HelloWorldService {
 		return Response.status(200).entity(output).build();
  
 	}
- 
+	
+	@POST
+	@Path("/post")
+	@Consumes("application/json")
+	@Produces("application/json")
+	public Response echoJson(String msg){
+		
+		return Response.status(200).build();
+	}
 }
