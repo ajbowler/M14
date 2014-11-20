44c3201aa7610f040fcf103f31adf76d8832de30
Andrew Bowler <ajbowler@iastate.edu>
2014-11-20 09:52:18 -0600

    Remove unnecessary whitespace

--- Java/REST/MpdConnection.java
+++ Java/REST/MpdConnection.java
@@ -1,12 +1,12 @@
 package com.m14.rest;
 
-//A smiple container class with contains infomation on an MPD Connection
+//A simple container class with contains infomation on an MPD Connection
 public class MpdConnection {
 
   //ip Address of connection
   private int ipAddress;
 
-        //port said connection uses
+  //port said connection uses
   private int port;
 
 }
