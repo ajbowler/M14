bd6d31b43d898e09b4b782e7dfe14fd28117e9c4
Andrew Bowler <ajbowler@iastate.edu>
2014-11-20 10:00:52 -0600

    Create getters and constructor for MpdConnection

--- Java/REST/MpdConnection.java
+++ Java/REST/MpdConnection.java
@@ -1,12 +1,29 @@
-package com.m14.rest;
-
-//A simple container class with contains infomation on an MPD Connection
+// data class that holds information for the user's connections
 public class MpdConnection {
 
-  //ip Address of connection
-  private int ipAddress;
-
-  //port said connection uses
+  private String host;
+  
   private int port;
+  
+  public MpdConnection(String host, int port) {
+    this.host = host;
+    this.port = port;
+  }
+  
+  public String getHost() {
+    return this.host;
+  }
+  
+  public int getPort() {
+    return this.port;
+  }
 
+  public Statement createStatement() {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+  //TODO
+  //find all the necessary properties needed for the Connection class
+  
 }
