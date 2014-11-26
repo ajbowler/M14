1f98d86bec176fd5f7df2858ee1e992bff7a75fd
Alex Lende <ajlende@gmail.com>
2014-11-26 11:23:08 -0600

    Revert "Dropdown menu"

--- WWW/js/components/MusicPlayer.jsx
+++ WWW/js/components/MusicPlayer.jsx
@@ -5,17 +5,22 @@
 var React = require('react/addons');
 var Panel = require('react-bootstrap/Panel');
 var ModalTrigger = require('react-bootstrap/ModalTrigger');
+var ModalButton = require('react-bootstrap/Button');
+var ProfileModal = require('./ProfileModal.jsx');
 var PlayerControls = require('./PlayerControls.jsx');
-var DropdownMenu = require('./DropdownMenu.jsx');
 
 var MusicPlayer = React.createClass({
-
   render: function() {
     return this.transferPropsTo(
       <div>
         <div>
           <img src="images/M14.svg" alt="M(usic)14" id="logo"/>
-          <DropdownMenu />
+          <ModalTrigger modal={<ProfileModal 
+            humanName='Andrew Bowler' 
+            username='ajbowler' 
+            email='ajbowler@iastate.edu'/>}>
+            <ModalButton id='profile' title='Replace this button with the dropdown' bsStyle='primary' bsSize='large' className='pull-right'>Profile</ModalButton>
+          </ModalTrigger>
         </div>
         <Panel className='panel-heading text-center'>
           <div>
