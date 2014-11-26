949cfd08b747aea8c334edd94aff0f7f753aba7d
Andrew Bowler <ajbowler@iastate.edu>
2014-11-26 12:36:07 -0600

    WIP added AJAX call for logging in

--- WWW/js/components/LoginModal.jsx
+++ WWW/js/components/LoginModal.jsx
@@ -2,14 +2,47 @@
 * @jsx React.DOM
 */
 
-var React = require('react');
+var React = require('react/addons');
 var Modal = require('react-bootstrap/Modal');
 var ModalTrigger = require('react-bootstrap/ModalTrigger');
 var UserInput = require('react-bootstrap/Input');
 var ModalButton = require('react-bootstrap/Button');
+var MusicPlayer = require('./MusicPlayer.jsx');
 
 var LoginModal = React.createClass({
-    render: function() {
+  propTypes: {
+    username: React.PropTypes.string,
+    email: React.PropTypes.string,
+    joinDate: React.PropTypes.string
+  },
+
+  handleLogin: function() {
+    var request = {
+      url: "http://proj-309-m14.cs.iastate.edu:8080:/login",
+      type: "GET",
+      contentType: "application/json",
+      cache: false,
+      dataType: "json",
+      data: JSON.stringify(userInfo)
+    };
+
+    $.get(request, function(result) {
+      var userInfo = JSON.parse(result);
+      this.props.username = userInfo.username;
+      this.props.email = userInfo.email;
+      this.props.joinDate = userInfo.joinDate;
+    });
+
+    React.renderComponent(
+      <MusicPlayer 
+        username={this.props.uesrname} 
+        email={this.props.email} 
+        joinDate={this.props.joinDate} />,
+      $('#container').get(0)
+    );
+  },
+
+  render: function() {
     return this.transferPropsTo(
       <Modal title='Login' backdrop={true} animation={false} >
         <div className='modal-body'>
@@ -17,25 +50,13 @@ var LoginModal = React.createClass({
             <form>
               <UserInput type='text'  placeholder='Username' label='Username' bsStyle='default' />
               <UserInput type='text' type='password' placeholder='Password' label='Password' bsStyle='default' />
-              <ModalButton bsStyle='default' type='submit' /* onClick= TODO */>Login</ModalButton>
+              <ModalButton bsStyle='default' type='submit' onClick={this.handleLogin; this.props.onRequestHide}>Login</ModalButton>
             </form>
             </div>
          </div>
       </Modal>
     );
-    }
-});
-
-
-// TODO:  Add REST request for username
-var LaunchButton = React.createClass({
-  render: function() {
-    return (
-      <ModalTrigger modal={<LoginModal />}>
-        <ModalButton bsStyle='primary' bsSize='large'>Login</ModalButton>
-      </ModalTrigger>
-    );
   }
 });
 
-module.exports = LaunchButton;
+module.exports = LoginModal;
