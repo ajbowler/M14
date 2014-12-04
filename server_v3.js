a6d65b6623a6269175db30cd1f23d0e38ca2aef0
Andrew Bowler <ajbowler@iastate.edu>
2014-12-04 17:11:47 -0600

    Refactor setupMPD and setupWS, add global list of MPD connections

--- server_v3.js
+++ server_v3.js
@@ -25,6 +25,12 @@ var WS = {
   serverActive: false
 };
 
+/**
+ * @desc The global list of MPD connections. Key-value pairs with host and port.
+ */
+var mpdConnectionList = {};
+
+
 MPD.prototype.setupMPD(host, port, pass) {
   var self = this;
 
@@ -47,15 +53,91 @@ MPD.prototype.setupMPD(host, port, pass) {
     });
   });
 
-  self.mpd.connection.on('end', function() {
+  self.netConnection.on('end', function() {
     console.log((new Date()) + ' MPD Connection Closed');
 
     // Notify the clients that the connection was closed
-    _.forEach(self.ws.clients, function (client) {
+    _.forEach(WS.clients, function (client) {
       client.sendUTF('MPD Connection Closed');
     });
 
-    self.mpd.isConnected = false;
-    self.mpd.isAuthenticated = false;
+    self.isConnected = false;
+    self.isAuthenticated = false;
+  });
+};
+
+function setupWS(port, mpd) {
+  console.log('setupWS called!');
+
+  port = port || 8007;
+
+  WS.httpServer = http.createServer(function(request, response) {
+    WS.serverActive = true;
+  });
+
+  WS.httpServer.listen(port, function() {
+    WS.serverActive = true;
+    console.log((new Date()) + ' Server is listening on port ' + port);
+  });
+
+  WS.wsServer = new WebSocketServer({httpServer: WS.httpServer});
+
+  WS.wsServer.on('request', function(r) {
+
+    var connection = r.accept('echo-protocol', r.origin);
+
+    // Specific id for this client & increment count
+    var id = WS.clientCount++;
+
+    connection.on('message', function(message) {
+
+      // The string message that was sent to us
+      // Parse msgString for the MPD to connect to.
+      message = JSON.parse(message);
+      var msgString = message.mpdCommand;
+      var selectedMPD = message.mpdHost;
+      console.log('msgString: ' + msgString);
+
+      /*
+      connections = {
+        '10.30.121.50:6600': mpdConnection
+        '0.0.0.0:6600': anotherMpdConnection,
+      };
+
+      var mpdConnection = connections[message.mpdHost].netDotConnection;
+
+      mpdConnection.write(things);
+      */
+
+      // Reconnect if necessicary
+      if (!mpd.isConnected) {
+        mpd.setupMPD(mpd.host, mpd.port, mpd.pass);
+      }
+
+      // Reauthenticate if necessicary
+      if (!mpd.isAuthenticated) {
+        mpd.connection.write('password ' + mpd.pass + '\n');
+        // TODO: Check to see if authentication was successful and set
+        // isAuthenticated to true. For now, enter password every time.
+      }
+
+      console.log((new Date()) + ' Sending MPD ' + msgString);
+
+      // Send the command to MPD
+      mpd.netConnection.write(msgString + '\n');
+
+    });
+
+    connection.on('close', function(reasonCode, description) {
+      delete WS.clients[id];
+      console.log((new Date()) + ' Peer ' + connection.remoteAddress + ' disconnected [' + id + ']');
+      WS.serverActive = false;
+    });
+
+    console.log((new Date()) + ' Connection accepted [' + id + ']');
+
+    // Store the connection method so we can loop through & contact all clients
+    WS.clients[id] = connection;
+
   });
 };
