013975787e92653e4e90e9d4c270000973a4a03e
nmont <nmont16@gmail.com>
2014-12-04 18:52:08 -0600

    WIP statusModal

--- WWW/js/components/StatusModal.jsx
+++ WWW/js/components/StatusModal.jsx
@@ -5,55 +5,62 @@
 var React = require('react');
 var Panel = require('react-bootstrap/Panel');
 
-
-var ws = new WebSocket('ws://65.110.226.243:8007', 'echo-protocol');
-var msg = "Getting Song Info..."
-
-function sendMessage(message) {
-  ws.send(message);
-}
-
-ws.addEventListener("message", function(e) {
-  // data we're getting back
-  msg = e.data;
-
-  if(msg.indexOf("Artist: ") > -1) {
-    var res1 = msg.split("Artist: ");
-    var Artist = res1[1].split("\n");
-  }
-  else {
-    var Artist = ["None"];
-  }
-
-  if(msg.indexOf("Title: ") > -1) {
-    var res2 = msg.split("Title: ");
-    var Title = res2[1].split("\n");
-  }
-  else var Title = ["None"];
-
-  if(msg.indexOf("Album: ") > -1) {
-    var res3 = msg.split("Album: ");
-    var Album = res3[1].split("\n");
-  }
-  else var Album = ["None"];
-
- // var listinfo = msg.split("-");
- // var artist = listinfo[0].split("/");
-  msg = "Artist: " + Artist[0] + "| \nAlbum: " + Album[0] + " | \nSong: " + Title[0];
-  document.getElementById('ws-messages').innerHTML += '<br>' + msg;
-});
-
-//sendMessage('status');
-
 var StatusModal = React.createClass({
-  getInfo: function () {
-    sendMessage('currentsong');
+
+  getInitialState: function() {
+
+    return {
+      status: "Getting Song Info"
+    };
   },
-  render: function() {
+
+  ws : new WebSocket('ws://65.110.226.243:8007', 'echo-protocol'),
+
+  this.ws.addEventListener("message", function(e) {
+    // data we're getting back
+    msg = e.data;
+
+    if(msg.indexOf("Artist: ") > -1) {
+      var res1 = msg.split("Artist: ");
+      var Artist = res1[1].split("\n");
+    }
+    else {
+      var Artist = ["None"];
+    }
+
+    if(msg.indexOf("Title: ") > -1) {
+      var res2 = msg.split("Title: ");
+      var Title = res2[1].split("\n");
+    }
+    else var Title = ["None"];
+
+    if(msg.indexOf("Album: ") > -1) {
+      var res3 = msg.split("Album: ");
+      var Album = res3[1].split("\n");
+    }
+    else var Album = ["None"];
+
+    var msg = "Artist: " + Artist[0] + " | \nAlbum: " + Album[0] + " | \nSong: " + Title[0];
+    this.setState({status:msg});
+    //document.getElementById('ws-messages').innerHTML += '<br>' + msg;
+
+  }),
+
+
+  sendMessage: function(message) {
+    this.ws.send(message);
+  },
+
+  getInfo: function () {
+    this.sendMessage('currentsong');
+  },
+
+  render : function() {
+    var app = this;
     return (
       <div>
         <Panel header={"Playing"} bsStyle="primary">
-        {msg}
+        {this.state.status}
         </Panel>
         <button onClick={this.getInfo}>Get Song</button>
       </div>
